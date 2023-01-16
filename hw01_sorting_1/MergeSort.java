1e0606f560f670a1d258d2652eda267f1bc51067
Lomtev Pavel <55400213+plomt@users.noreply.github.com>
2023-01-17 00:38:19 +0300

    Create MergeSort.java
    hw01

---
+++ hw01_sorting_1/MergeSort.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+
+public class MergeSort {
+    public static void mergeSort(int[] array, int left, int right) {
+        if (left + 1 >= right)
+            return;
+        else {
+            int middle = (left + right) / 2;
+            mergeSort(array, left, middle);
+            mergeSort(array, middle, right);
+            merge(array, left, middle, right);
+        }
+    }
+
+    public static void merge(int[] array, int left, int middle, int right) {
+        int i = left;
+        int j = middle;
+        int k = 0;
+        int[] result = new int[right - left];
+
+        while (i < middle && j < right) {
+            if (array[i] <= array[j]) {
+                result[k] = array[i];
+                i++;
+            }
+            else {
+                result[k] = array[j];
+                j++;
+            }
+            k++;
+        }
+        for (;i < middle; i++, k++) {
+            result[k] = array[i];
+        }
+        for (;j < right; j++, k++) {
+            result[k] = array[j];
+        }
+
+        for (int idx = 0; idx < right - left; idx++) {
+            array[left + idx] = result[idx];
+        }
+    }
+
+    public static void main(String... args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();;
+        int[] values = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            values[i] = scanner.nextInt();
+        }
+
+        mergeSort(values, 0, n);
+
+        for (int val: values) {
+            System.out.print(val + " ");
+        }
+    }
+}
